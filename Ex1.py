7ba637fa01c8e722336d745e8d352a5ccea8212c
Hugo Siriphong <e2235086@cmaisonneuve.qc.ca>
2024-05-13 22:10:37 -0400

    Écrire et charger les nombres complexes du fichier json à csv. Résultat

--- Ex1.py
+++ Ex1.py
@@ -12,3 +12,12 @@ def ecrire_csv_nombres_complexes(nombres_complexes, chemin_fichier):
         ecrivain.writerow(['reel', 'imaginaire'])
         for nombre in nombres_complexes:
             ecrivain.writerow([nombre[0], nombre[1]])
+
+chemin_json = 'data.json'
+chemin_csv = 'output.csv'
+
+nombres_complexes = charger_nombres_complexes(chemin_json)
+
+ecrire_csv_nombres_complexes(nombres_complexes, chemin_csv)
+
+print("Conversion terminée. Les nombres complexes ont été écrits dans", chemin_csv)
