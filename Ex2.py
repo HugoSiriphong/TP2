3f6f03ea23d2206adf51cbc694f6ed5e86568bf1
e6254213 <e6254213@cmaisonneuve.qc.ca>
2024-05-14 11:44:22 -0400

    Création du codage pour la lecture de chaque ligne du fichier CSV

--- Ex2.py
+++ Ex2.py
@@ -1,5 +1,17 @@
 import csv
 
-def lire_pokemon_csv(nom_fichier):
+def lire_pokemon_csv(pokemon):
+
+    pokemon_stats = {}
+
+    with open(pokemon, newline='') as csvfile:
+        lire = csv.reader(csvfile)
+        for i in lire:  # i signifie rangée
+            nom_pokemon = i[0]
+            stats_str = i[1:]
+            stats_int = [int(stat) for stat in stats_str]
+            pokemon_stats[nom_pokemon] = stats_int
+
+    return pokemon_stats
 
 
