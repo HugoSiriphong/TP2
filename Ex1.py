b2de0fba9280c835185205730252eb5dd5cc550a
Hugo Siriphong <e2235086@cmaisonneuve.qc.ca>
2024-05-13 21:45:22 -0400

    Fonction pour écrire les chiffres complexes dans le fichier csv

--- Ex1.py
+++ Ex1.py
@@ -5,3 +5,10 @@ def charger_nombres_complexes(chemin_fichier):
     with open(chemin_fichier, 'r') as f:
         donnees = json.load(f)
     return donnees
+
+def ecrire_csv_nombres_complexes(nombres_complexes, chemin_fichier):
+    with open(chemin_fichier, 'w', newline='') as f:
+        ecrivain = csv.writer(f)
+        ecrivain.writerow(['reel', 'imaginaire'])
+        for nombre in nombres_complexes:
+            ecrivain.writerow([nombre[0], nombre[1]])
