ee79a9a1db46a7154323b6753c2c1afa1fa44316
e6254213 <e6254213@cmaisonneuve.qc.ca>
2024-05-14 12:11:33 -0400

    Finalisation du codage pour la lecture de chaque ligne du fichier CSV

--- Ex2.py
+++ Ex2.py
@@ -2,19 +2,20 @@ import csv
 
 def lire_pokemon_csv(pokemon):
 
-
     pokemon_stats = {}
 
     with open(pokemon, newline='') as csvfile:
         lire = csv.reader(csvfile)
-        for i in lire:  # i signifie rangée
+        for i in lire:  # i = rangée
             nom_pokemon = i[0]
             stats_str = i[1:]
             stats_int = [int(stat) for stat in stats_str]
-            pokemon_stats[nom_pokemon] = stats_int
+            stats_format ="".join([f"({stat_label} {stat_value})" for stat_label, stat_value in zip(["HP", "ATTAQUE", "DEFENSE", "SP ATTAQUE", "SP DEFENSE", "VITESSE"], stats_int)])
+            # FORMATAGE DU DICTIONNAIRE
+            pokemon_stats[nom_pokemon] = stats_format
 
     return pokemon_stats
 
-
-
-
+fichier_csv = "pokemon.csv"
+donnees_pokemon = lire_pokemon_csv(fichier_csv)
+print(donnees_pokemon)
